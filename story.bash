8681a3774098185defcf97f28673c765238c67ff
Alexey Melezhik <melezhik@gmail.com>
2016-06-11 00:08:04 +0300

    first version

--- story.bash
+++ story.bash
@@ -4,32 +4,36 @@ app_user=$(config app_user)
 app_source_url=$(config app_source_url)
 git_branch=$(config git_branch)
 
-useradd $app_user
+sudo useradd -m --shell `which bash` $app_user
 
-cpanm Ubic || exit 1
-cpanm Starman || exit 1
+sudo cpanm -q Ubic Ubic::Service::Plack || exit 1
+sudo cpanm -q Starman || exit 1
 
-sudo -u $app_user mkdir -p $app_dir
+sudo mkdir -p $app_dir
+sudo chown $app_user $app_dir
+
+sudo ubic-admin setup --batch-mode --quiet
+sudo ubic stop perl-app
 
 if test -d $app_dir/.git; then
-  sudo -u $app_user -E bash --login -c "cd $app_dir && git pull && carton install --deployment"
+  sudo -u $app_user bash --login -c "cd $app_dir && git pull && carton install --deployment"
 else
-  sudo -u $app_user -E bash --login -c "git clone --branch $git_branch $app_source_url $app_dir \
-  cd $app_dir && carton install --deployment" 
+  sudo -u $app_user bash --login -c "git clone --branch $git_branch $app_source_url $app_dir \
+  && cd $app_dir && carton install --deployment" 
 fi
 
 
-mkdir -p  /etc/ubic/service/
+sudo mkdir -p  /etc/ubic/service/
 
-cat > /etc/ubic/service/dancer_app
+cat << EOF > /tmp/dancer_app.ubic.conf  
 
 use parent qw(Ubic::Service::Plack);
  
 # if your application is not installed in @INC path:
 sub start {
-    my $self = shift;
-    $ENV{PERL5LIB} = "$app_dir/local/lib";
-    $self->SUPER::start(@_);
+    my \$self = shift;
+    \$ENV{PERL5LIB} = "$app_dir/local/lib/perl5";
+    \$self->SUPER::start(@_);
 }
  
 __PACKAGE__->new(
@@ -39,6 +43,11 @@ __PACKAGE__->new(
     user => "$app_user",
 );
 
+EOF
 
-echo install-ok
+sudo cp /tmp/dancer_app.ubic.conf /etc/ubic/service/perl-app
+
+sudo ubic start perl-app
+
+sudo ubic status perl-app && echo install-ok
 
