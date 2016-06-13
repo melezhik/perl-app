a4fdf23592c40fe13cd1a58abe2cf6dde61eacbd
Alexey Melezhik <melezhik@gmail.com>
2016-06-13 14:56:09 +0300

    checkout branch

--- story.bash
+++ story.bash
@@ -19,7 +19,7 @@ sudo ubic-admin setup --batch-mode --quiet
 sudo ubic stop perl-app
 
 if test -d $app_dir/.git; then
-  sudo -u $app_user bash --login -c "cd $app_dir && git pull && carton install --deployment" || exit 1
+  sudo -u $app_user bash --login -c "cd $app_dir && git checkout $git_branch && git pull && carton install --deployment" || exit 1
 else
   sudo -u $app_user bash --login -c "git clone --branch $git_branch $app_source_url $app_dir \
   && cd $app_dir && carton install --deployment" || exit 1
